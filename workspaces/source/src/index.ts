d8a8b8e3b2b998cd50699d70bf78588b7c82966b
danwithabox <danwithabox@gmail.com>
2024-05-08 18:50:52 +0200

    minor cleanup

--- workspaces/source/src/index.ts
+++ workspaces/source/src/index.ts
@@ -1,3 +1,3 @@
-export { type _TupleSimplify as TupleSimplify } from "./tuple-simplify.js";
+export { type _TupleSimplify } from "./tuple-simplify.js";
 export { tuple, tupleOf, type InferConstTuple } from "./tuple.js";
 export { tupleUnique, tupleUniqueOf, tupleExhaustiveOf, type InTuple, type UniqueTuple, type DepletingTuple } from "./tuple-unique.js";
