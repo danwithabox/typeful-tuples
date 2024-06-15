283246c990091d32d6e36a07418b98275ad45af9
danwithabox <danwithabox@gmail.com>
2024-06-15 11:45:25 +0200

    tests rewritten: tuple-exhaustive.test.ts, tuple-exhaustive.test-d.ts

--- test/tuple-exhaustive-of.test-d.ts
+++ test/tuple-exhaustive-of.test-d.ts
@@ -1,194 +1,30 @@
 import { expectTypeOf, test } from "vitest";
-import { tuple, tupleOf, tupleExhaustiveOf } from "../src/index";
+import { tupleExhaustiveOf } from "../src/index";
 
-type Allowed = "1" | "2" | "3";
+type Allowed = "foo" | "bar" | "baz";
 
-test(`tupleExhaustiveOf() cannot accept non-const input`, () => {
-    const input = [1];
-    // @ts-expect-error unacceptable input
+test(`tupleExhaustiveOf() returns the input unchanged, non-const literal`, () => {
+    const output = tupleExhaustiveOf<Allowed>()(["foo", "bar", "baz"]);
+    expectTypeOf(output).toEqualTypeOf<["foo", "bar", "baz"]>();
+});
+
+//#region NOTE: this is not an intended use-case, therefore I shouldn't really test it, probably
+
+// test(`tupleExhaustiveOf() returns the input unchanged, non-const variable`, () => {
+//     const input: Allowed[] = ["foo"];
+//     const output = tupleExhaustiveOf<Allowed>()(input);
+//     expectTypeOf(output).toEqualTypeOf<Allowed[]>();
+// });
+
+//#endregion
+
+test(`tupleExhaustiveOf() returns the input unchanged, as const literal`, () => {
+    const output = tupleExhaustiveOf<Allowed>()(["foo", "bar", "baz"] as const);
+    expectTypeOf(output).toEqualTypeOf<readonly ["foo", "bar", "baz"]>();
+});
+
+test(`tupleExhaustiveOf() returns the input unchanged, as const variable`, () => {
+    const input = ["foo", "bar", "baz"] as const;
     const output = tupleExhaustiveOf<Allowed>()(input);
-
-    // @ts-expect-error invalid
-    expectTypeOf(output).toEqualTypeOf<typeof input>();
-});
-
-test(`tupleExhaustiveOf() returns the input type unchanged, as const`, () => {
-    {
-        const input = ["1", "2", "3"] as const;
-        const output = tupleExhaustiveOf<Allowed>()(input);
-        expectTypeOf(output).toEqualTypeOf<typeof input>();
-    }
-    {
-        const input = tuple(["1", "2", "3"]);
-        const output = tupleExhaustiveOf<Allowed>()(input);
-        expectTypeOf(output).toEqualTypeOf<typeof input>();
-    }
-    {
-        const input = tupleOf<Allowed>()(["1", "2", "3"]);
-        const output = tupleExhaustiveOf<Allowed>()(input);
-        expectTypeOf(output).toEqualTypeOf<typeof input>();
-    }
-    {
-        const output = tupleExhaustiveOf<Allowed>()(["1", "2", "3"]);
-        expectTypeOf(output).toEqualTypeOf<["1", "2", "3"]>();
-    }
-
-    {
-        const curried = tupleExhaustiveOf<Allowed>();
-        const output = curried(["1", "2", "3"]);
-        expectTypeOf(output).toEqualTypeOf<["1", "2", "3"]>();
-    }
-    {
-        const curried = tupleExhaustiveOf<Allowed>();
-        const output = curried(["1", "2", "3"] as const);
-        expectTypeOf(output).toEqualTypeOf<readonly ["1", "2", "3"]>();
-    }
-
-    {
-        const output = tupleExhaustiveOf<Allowed>()(["1", "2", "3"] as const);
-        expectTypeOf(output).toEqualTypeOf<readonly ["1", "2", "3"]>();
-    }
-});
-
-test(`tupleExhaustiveOf() checks for non-unique elements`, () => {
-    {
-        const input = ["1", "2", "3", "1"] as const;
-        // @ts-expect-error invalid input
-        const output = tupleExhaustiveOf<Allowed>()(input);
-        expectTypeOf(output).toEqualTypeOf<typeof input>();
-    }
-    {
-        const output = tupleExhaustiveOf<Allowed>()([
-            "1",
-            "2",
-            "3",
-            // @ts-expect-error invalid input
-            "1",
-        ]);
-        expectTypeOf(output).toEqualTypeOf<["1", "2", "3", "1"]>();
-    }
-});
-
-test(`tupleExhaustiveOf() checks for not allowed elements`, () => {
-    const output = tupleExhaustiveOf<Allowed>()([
-        "1",
-        "2",
-        // @ts-expect-error invalid input
-        "4",
-    ]);
-    expectTypeOf(output).toEqualTypeOf<readonly ("" | Allowed)[]>();
-
-    tupleExhaustiveOf<Allowed>()([
-        // @ts-expect-error invalid input
-        "4",
-        "2",
-        "3",
-    ]);
-
-    tupleExhaustiveOf<Allowed>()([
-        // @ts-expect-error invalid input
-        "4",
-        "2",
-        // @ts-expect-error invalid input
-        "4",
-    ]);
-
-    tupleExhaustiveOf<Allowed>()([
-        // @ts-expect-error invalid input
-        "4",
-        // @ts-expect-error invalid input
-        "4",
-        // @ts-expect-error invalid input
-        "4",
-    ]);
-
-    {
-        const input = ["4", "2", "4"] as const;
-        // @ts-expect-error invalid input
-        const output = tupleExhaustiveOf<Allowed>()(input);
-    }
-});
-
-test(`tupleExhaustiveOf() checks for exhaustiveness`, () => {
-    {
-        const output = tupleExhaustiveOf<Allowed>()(
-            // @ts-expect-error Source has 2 element(s) but target requires 3. ts(2345)
-            ["1", "2"],
-        );
-        expectTypeOf(output).toEqualTypeOf<["1", "2"]>();
-    }
-
-    {
-        const input = ["1", "2"] as const;
-        const output = tupleExhaustiveOf<Allowed>()(
-            // @ts-expect-error Source has 2 element(s) but target requires 3. ts(2345)
-            input,
-        );
-        expectTypeOf(output).toEqualTypeOf<readonly ["1", "2"]>();
-    }
-});
-
-test(`tupleExhaustiveOf autocomplete checks`, () => {
-    type Keys = "foo" | "bar" | "baz";
-
-    const curried = tupleExhaustiveOf<Keys>();
-
-    {
-        // All options are used up without error, so each element can only be the element itself
-        curried(["foo", "bar", "baz"]);
-        expectTypeOf(curried<["foo", "bar", "baz"]>).parameter(0).toHaveProperty(0).toEqualTypeOf<"foo">();
-        expectTypeOf(curried<["foo", "bar", "baz"]>).parameter(0).toHaveProperty(1).toEqualTypeOf<"bar">();
-        expectTypeOf(curried<["foo", "bar", "baz"]>).parameter(0).toHaveProperty(2).toEqualTypeOf<"baz">();
-    }
-
-    {
-        // @ts-expect-error Bring up autocomplete inside the parameter `""`, the options should be `"bar"` and `"baz"` and `"foo"`
-        curried([""]);
-        expectTypeOf(curried<[""]>).parameter(0).toHaveProperty(0).toEqualTypeOf<"foo" | "bar" | "baz">();
-    }
-    {
-        // @ts-expect-error Bring up autocomplete inside the parameter `""`, the options should be `"bar"` and `"foo"`
-        curried(["baz", ""]);
-        expectTypeOf(curried<["baz", ""]>).parameter(0).toHaveProperty(0).toEqualTypeOf<"baz">();
-        expectTypeOf(curried<["baz", ""]>).parameter(0).toHaveProperty(1).toEqualTypeOf<"foo" | "bar">();
-    }
-    {
-        // @ts-expect-error Bring up autocomplete inside the parameter `""`, the options should be `"bar"` and `"foo"`
-        curried(["", "baz"]);
-        expectTypeOf(curried<["", "baz"]>).parameter(0).toHaveProperty(0).toEqualTypeOf<"foo" | "bar">();
-        expectTypeOf(curried<["", "baz"]>).parameter(0).toHaveProperty(1).toEqualTypeOf<"baz">();
-    }
-
-    {
-        // @ts-expect-error Bring up autocomplete inside the parameter `""`, the options should be `"bar"`
-        curried(["", "bar", "baz"]);
-        expectTypeOf(curried<["", "bar", "baz"]>).parameter(0).toHaveProperty(0).toEqualTypeOf<"foo">();
-        expectTypeOf(curried<["", "bar", "baz"]>).parameter(0).toHaveProperty(1).toEqualTypeOf<"bar">();
-        expectTypeOf(curried<["", "bar", "baz"]>).parameter(0).toHaveProperty(2).toEqualTypeOf<"baz">();
-    }
-
-    {
-        // @ts-expect-error Bring up autocomplete inside the parameter `""`, the options should be `"bar"`
-        curried(["foo", "", "baz"]);
-        expectTypeOf(curried<["foo", "", "baz"]>).parameter(0).toHaveProperty(0).toEqualTypeOf<"foo">();
-        expectTypeOf(curried<["foo", "", "baz"]>).parameter(0).toHaveProperty(1).toEqualTypeOf<"bar">();
-        expectTypeOf(curried<["foo", "", "baz"]>).parameter(0).toHaveProperty(2).toEqualTypeOf<"baz">();
-    }
-
-    {
-        // @ts-expect-error Bring up autocomplete inside the parameter `""`, the options should be `"baz"`
-        curried(["foo", "bar", ""]);
-        expectTypeOf(curried<["foo", "bar", ""]>).parameter(0).toHaveProperty(0).toEqualTypeOf<"foo">();
-        expectTypeOf(curried<["foo", "bar", ""]>).parameter(0).toHaveProperty(1).toEqualTypeOf<"bar">();
-        expectTypeOf(curried<["foo", "bar", ""]>).parameter(0).toHaveProperty(2).toEqualTypeOf<"baz">();
-    }
-
-    {
-        // @ts-expect-error Bring up autocomplete inside the parameter `""`, the options should be `"baz"`
-        curried(["foo", "bar", "baz", ""]);
-        expectTypeOf(curried<["foo", "bar", "baz", ""]>).parameter(0).toHaveProperty(0).toEqualTypeOf<"foo">();
-        expectTypeOf(curried<["foo", "bar", "baz", ""]>).parameter(0).toHaveProperty(1).toEqualTypeOf<"bar">();
-        expectTypeOf(curried<["foo", "bar", "baz", ""]>).parameter(0).toHaveProperty(2).toEqualTypeOf<"baz">();
-        expectTypeOf(curried<["foo", "bar", "baz", ""]>).parameter(0).toHaveProperty(3).toEqualTypeOf<never>();
-    }
+    expectTypeOf(output).toEqualTypeOf<readonly ["foo", "bar", "baz"]>();
 });
--- test/tuple-exhaustive-of.test.ts
+++ test/tuple-exhaustive-of.test.ts
@@ -1,19 +1,397 @@
 import { describe, expect, test } from "vitest";
 import { tupleExhaustiveOf } from "../src/index";
+import { virtualTs } from "../vitest/utils/vitest-virtual-typescript";
 
 describe(`tupleExhaustiveOf()`, () => {
+    const { tooling: { runQueryOnVirtualFile, }, } = virtualTs;
 
     test(`returns the input value unchanged`, () => {
         const input = [1, 2, 3];
         const output = tupleExhaustiveOf<(typeof input)[number]>()(input);
-
         expect(output).toEqual(input);
     });
 
-    describe(`IDE / language server developer experience`, () => {
+    describe(`developer experience, expected feedback in IDE`, () => {
 
-        test(`autocomplete shows the correct options`, () => {
+        test(`diagnostics result, when provided tuple is \`["foo", ""]\`: Type '""' is not assignable to type 'Allowed'.`, () => {
+            const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["foo", ""]);
+            `);
+
+            expect(result).toHaveSemanticDiagnostics([{
+                category:    "Error",
+                code:        2322,
+                messageText: `Type '""' is not assignable to type '"bar" | "baz"'.`,
+                lines:       [4],
+            }]);
+        });
+
+        test(`diagnostics result, when provided tuple is \`["foo", "err"]\`: Type '"err"' is not assignable to type 'Allowed'.`, () => {
+            const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["foo", "err"]);
+            `);
+
+            expect(result).toHaveSemanticDiagnostics([{
+                category:    "Error",
+                code:        2322,
+                messageText: `Type '"err"' is not assignable to type '"" | Allowed'.`,
+                lines:       [4],
+            }]);
+        });
+
+        test(`diagnostics result, when provided tuple is \`["err", "foo"]\`: Type '"err"' is not assignable to type 'Allowed'.`, () => {
+            const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["err", "foo"]);
+            `);
+
+            expect(result).toHaveSemanticDiagnostics([{
+                category:    "Error",
+                code:        2322,
+                messageText: `Type '"err"' is not assignable to type '"" | Allowed'.`,
+                lines:       [4],
+            }]);
+        });
+
+        test(`diagnostics result, when provided variable is \`["foo", "err", "baz"]\`: Type '"err"' is not assignable to type 'Allowed'.`, () => {
+            const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()([
+                    "foo",
+                    "err",
+                    "baz",
+                ]);
+            `);
+
+            expect(result).toHaveSemanticDiagnostics([
+                {
+                    category:    "Error",
+                    code:        2322,
+                    messageText: `Type '"err"' is not assignable to type '"" | Allowed'.`,
+                    lines:       [6],
+                },
+            ]);
+        });
+
+        test(`diagnostics result, when provided variable is \`const input = ["foo", "err"] as const\`: Type '"err"' is not assignable to type 'Allowed'.`, () => {
+            const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                const input = ["foo", "err"] as const;
+                tupleExhaustiveOf<Allowed>()(input);
+            `);
+
+            expect(result).toHaveSemanticDiagnostics([
+                {
+                    category:    "Error",
+                    code:        2345,
+                    messageText: `Argument of type 'readonly ["foo", "err"]' is not assignable to parameter of type 'readonly ("" | Allowed)[]'.`,
+                    lines:       [5],
+                },
+                {
+                    category:    "Error",
+                    code:        2322,
+                    messageText: `Type '"foo" | "err"' is not assignable to type '"" | Allowed'.`,
+                    lines:       [5],
+                },
+                {
+                    category:    "Error",
+                    code:        2322,
+                    messageText: `Type '"err"' is not assignable to type '"" | Allowed'.`,
+                    lines:       [5],
+                },
+            ]);
+        });
+
+        test(`diagnostics result, when provided variable is \`const input = ["foo", "err"]\`: Type 'string' is not assignable to type 'Allowed'.`, () => {
+            const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                const input = ["foo", "err"];
+                tupleExhaustiveOf<Allowed>()(input);
+            `);
+
+            expect(result).toHaveSemanticDiagnostics([
+                {
+                    category:    "Error",
+                    code:        2345,
+                    messageText: `Argument of type 'string[]' is not assignable to parameter of type 'readonly ("" | Allowed)[]'.`,
+                    lines:       [5],
+                },
+                {
+                    category:    "Error",
+                    code:        2322,
+                    messageText: `Type 'string' is not assignable to type '"" | Allowed'.`,
+                    lines:       [5],
+                },
+            ]);
+        });
+
+        test(`autocomplete result, when provided tuple is \`["foo", ""]\`: ["bar", "baz"]`, () => {
+            const result = runQueryOnVirtualFile.getCompletionsAtPosition("../index.ts", ({ $c, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["foo", "${$c}"]);
+            `);
+
+            expect(result).toHaveCompletions(["bar", "baz"]);
+        });
+
+        test(`autocomplete result, when provided tuple is \`["", "bar"]\`: ["foo", "baz"]`, () => {
+            const result = runQueryOnVirtualFile.getCompletionsAtPosition("../index.ts", ({ $c, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["${$c}", "bar"]);
+            `);
+
+            expect(result).toHaveCompletions(["foo", "baz"]);
+        });
+
+        test(`autocomplete result, when provided tuple is \`["foo", "bar", ""]\`: ["baz"]`, () => {
+            const result = runQueryOnVirtualFile.getCompletionsAtPosition("../index.ts", ({ $c, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["foo", "bar", "${$c}"]);
+            `);
+
+            expect(result).toHaveCompletions(["baz"]);
+        });
+
+        test(`autocomplete result, when provided tuple is \`["foo", "", "baz"]\`: ["bar"]`, () => {
+            const result = runQueryOnVirtualFile.getCompletionsAtPosition("../index.ts", ({ $c, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["foo", "${$c}", "baz"]);
+            `);
+
+            expect(result).toHaveCompletions(["bar"]);
+        });
+
+        test(`autocomplete result, when provided tuple is \`["", "bar", "baz"]\`: ["foo"]`, () => {
+            const result = runQueryOnVirtualFile.getCompletionsAtPosition("../index.ts", ({ $c, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["${$c}", "bar", "baz"]);
+            `);
+
+            expect(result).toHaveCompletions(["foo"]);
+        });
+
+        test(`autocomplete result, when provided tuple is \`["foo", "bar", "baz", ""]\`: []`, () => {
+            const result = runQueryOnVirtualFile.getCompletionsAtPosition("../index.ts", ({ $c, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["foo", "bar", "baz", "${$c}"]);
+            `);
+
+            expect(result).toHaveCompletions([]);
+        });
+
+        test(`autocomplete result, when provided tuple is \`["", "foo", "bar", "baz"]\`: []`, () => {
+            const result = runQueryOnVirtualFile.getCompletionsAtPosition("../index.ts", ({ $c, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["${$c}", "foo", "bar", "baz"]);
+            `);
+
+            expect(result).toHaveCompletions([]);
+        });
+
+        test(`autocomplete result, when provided tuple is \`["foo", "bar", "baz"]\`, and checking first param: ["foo"]`, () => {
+            const result = runQueryOnVirtualFile.getCompletionsAtPosition("../index.ts", ({ $c, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["foo${$c}", "bar", "baz"]);
+            `);
+
+            expect(result).toHaveCompletions(["foo"]);
+        });
+
+        test(`autocomplete result, when provided tuple is \`["foo", "bar", "baz"]\`, and checking second param: ["bar"]`, () => {
+            const result = runQueryOnVirtualFile.getCompletionsAtPosition("../index.ts", ({ $c, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["foo", "bar${$c}", "baz"]);
+            `);
+
+            expect(result).toHaveCompletions(["bar"]);
+        });
+
+        test(`autocomplete result, when provided tuple is \`["foo", "bar", "baz"]\`, and checking third param: ["baz"]`, () => {
+            const result = runQueryOnVirtualFile.getCompletionsAtPosition("../index.ts", ({ $c, $imports, }) => /* ts */`
+                import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                type Allowed = "foo" | "bar" | "baz";
+                tupleExhaustiveOf<Allowed>()(["foo", "bar", "baz${$c}"]);
+            `);
+
+            expect(result).toHaveCompletions(["baz"]);
+        });
+
+        describe(`uniqueness checks`, () => {
+
+            test(`diagnostics result, when provided non-unique tuple is \`["foo", "foo"]\`: there is no error, due to deliberately not having a uniqueness check`, () => {
+                const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                    import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                    type Allowed = "foo" | "bar" | "baz";
+                    tupleExhaustiveOf<Allowed>()([
+                        "foo",
+                        "foo",
+                    ]);
+                `);
+
+                expect(result).toHaveSemanticDiagnostics([
+                    {
+                        category:    "Error",
+                        code:        2322,
+                        lines:       [6],
+                        messageText: "Type 'string' is not assignable to type 'never'.",
+                    },
+                ]);
+            });
+
+            test(`diagnostics result, when provided tuple is \`["foo", "foo", "err"]\`: Type '"err"' is not assignable to type 'Allowed'.`, () => {
+                const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                    import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                    type Allowed = "foo" | "bar" | "baz";
+                    tupleExhaustiveOf<Allowed>()(["foo", "foo", "err"]);
+                `);
+
+                expect(result).toHaveSemanticDiagnostics([{
+                    category:    "Error",
+                    code:        2322,
+                    messageText: `Type '"err"' is not assignable to type '"" | Allowed'.`,
+                    lines:       [4],
+                }]);
+            });
+
+            test(`diagnostics result, when provided tuple is \`["err", "foo", "foo"]\`: Type '"err"' is not assignable to type 'Allowed'.`, () => {
+                const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                    import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                    type Allowed = "foo" | "bar" | "baz";
+                    tupleExhaustiveOf<Allowed>()(["err", "foo", "foo"]);
+                `);
+
+                expect(result).toHaveSemanticDiagnostics([{
+                    category:    "Error",
+                    code:        2322,
+                    messageText: `Type '"err"' is not assignable to type '"" | Allowed'.`,
+                    lines:       [4],
+                }]);
+            });
+
+            test(`autocomplete result, when provided non-unique tuple is \`["foo", "foo", ""]\`: ["bar", "baz"]`, () => {
+                const result = runQueryOnVirtualFile.getCompletionsAtPosition("../index.ts", ({ $c, $imports, }) => /* ts */`
+                    import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                    type Allowed = "foo" | "bar" | "baz";
+                    tupleExhaustiveOf<Allowed>()(["foo", "foo", "${$c}"]);
+                `);
+
+                expect(result).toHaveCompletions(["bar", "baz"]);
+            });
 
         });
+
+        describe(`exhaustiveness checks`, () => {
+
+            test(`diagnostics result, when provided tuple is \`["foo"]\`: Source has 1 element(s) but target requires 2.`, () => {
+                const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                    import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                    type Allowed = "foo" | "bar" | "baz";
+                    tupleExhaustiveOf<Allowed>()(["foo"]);
+                `);
+
+                expect(result).toHaveSemanticDiagnostics([
+                    {
+                        category:    "Error",
+                        code:        2345,
+                        lines:       [4],
+                        messageText: `Argument of type '["foo"]' is not assignable to parameter of type 'readonly ["foo", "bar" | "baz"]'.`,
+                    },
+                    {
+                        category:    "Error",
+                        code:        2618,
+                        lines:       [4],
+                        messageText: `Source has 1 element(s) but target requires 2.`,
+                    },
+                ]);
+            });
+
+            test(`diagnostics result, when provided tuple is \`["foo", "baz"]\`: Source has 2 element(s) but target requires 3.`, () => {
+                const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                    import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                    type Allowed = "foo" | "bar" | "baz";
+                    tupleExhaustiveOf<Allowed>()(["foo", "baz"]);
+                `);
+
+                expect(result).toHaveSemanticDiagnostics([
+                    {
+                        category:    "Error",
+                        code:        2345,
+                        lines:       [4],
+                        messageText: `Argument of type '["foo", "baz"]' is not assignable to parameter of type 'readonly ["foo", "baz", "bar"]'.`,
+                    },
+                    {
+                        category:    "Error",
+                        code:        2618,
+                        lines:       [4],
+                        messageText: `Source has 2 element(s) but target requires 3.`,
+                    },
+                ]);
+            });
+
+            test(`diagnostics result, when provided tuple is \`[]\`: Source has 0 element(s) but target requires 1.`, () => {
+                const result = runQueryOnVirtualFile.getSemanticDiagnostics("../index.ts", ({ $l, $imports, }) => /* ts */`
+                    import { tupleExhaustiveOf } from "${$imports["./src/index"]}";
+
+                    type Allowed = "foo" | "bar" | "baz";
+                    tupleExhaustiveOf<Allowed>()([]);
+                `);
+
+                expect(result).toHaveSemanticDiagnostics([
+                    {
+                        category:    "Error",
+                        code:        2345,
+                        lines:       [4],
+                        messageText: `Argument of type '[]' is not assignable to parameter of type 'readonly [Allowed]'.`,
+                    },
+                    {
+                        category:    "Error",
+                        code:        2618,
+                        lines:       [4],
+                        messageText: `Source has 0 element(s) but target requires 1.`,
+                    },
+                ]);
+            });
+
+        });
+
     });
 });
